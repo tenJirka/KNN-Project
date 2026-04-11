6dbf161a1fa39f9fec1808c00fa605ddf228c5cf
Marek Halamka <marek.halamka1@seznam.cz>
2026-04-11 15:21:36 +0200

    training: implemented validation split from full training set

--- train/train.py
+++ train/train.py
@@ -9,7 +9,9 @@ from PIL import Image
 from pytorch_lightning.callbacks import ModelCheckpoint
 from pytorch_metric_learning import losses, miners
 from shared import GenericReIDModel, ReIDLightningModel
-from torch.utils.data import DataLoader, Dataset
+from torch.utils.data import DataLoader, Dataset, Subset
+import random
+from util import DatasetSubset
 
 # For faster learning
 torch.set_float32_matmul_precision("medium")
@@ -52,11 +54,79 @@ class VeRiDataset(Dataset):
         return image, label, cam_id
 
 
+def get_veri_split(train_dataset: VeRiDataset, veri_percent=0.1, seed=42):
+    """Helper function to get a random subset of the VeRi dataset for validation"""
+    all_indices = list(train_dataset.id_to_class.keys())
+    random.seed(seed)
+    random.shuffle(all_indices)
+
+    split_point = int(len(all_indices) * (1.0 - veri_percent))
+    train_ids = all_indices[:split_point]
+    val_ids = all_indices[split_point:]
+
+    train_indices = [
+        i
+        for i, img_name in enumerate(train_dataset.img_names)
+        if int(img_name.split("_")[0]) in train_ids
+    ]
+
+    val_indices = [
+        i
+        for i, img_name in enumerate(train_dataset.img_names)
+        if int(img_name.split("_")[0]) in val_ids
+    ]
+
+    return train_indices, val_indices
+
+
 if __name__ == "__main__":
     # Number of ids in VeRi train dataset
-    NUM_VERI_TRAIN_CLASSES = 576
+    full_train_dataset = VeRiDataset(
+        img_dir="../datasets/VeRi/image_train/", transform=None
+    )
+    VAL_PERCENT = 0.1
+
+    # TODO: Do more research on transformations
+    train_transform = T.Compose(
+        [
+            T.Resize((256, 256)),
+            T.RandomHorizontalFlip(),
+            T.ToTensor(),
+            T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ]
+    )
+
+    validation_transform = T.Compose(
+        [
+            T.Resize((256, 256)),
+            T.ToTensor(),
+            T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ]
+    )
+
+    print(
+        "Number of classes in full VeRi train dataset:",
+        len(full_train_dataset.id_to_class),
+    )
+    train_subset_ids, val_subset_ids = get_veri_split(
+        full_train_dataset, veri_percent=VAL_PERCENT
+    )
+    # NUM_VERI_TRAIN_CLASSES = len(train_subset.dataset.id_to_class)
+    # print(f"Number of classes in VeRi train subset: {NUM_VERI_TRAIN_CLASSES}")
 
     model = GenericReIDModel("resnet50")
+    train_dataset = DatasetSubset(
+        whole_dataset=full_train_dataset,
+        subset_indices=train_subset_ids,
+        transform=train_transform,
+    )
+    val_dataset = DatasetSubset(
+        whole_dataset=full_train_dataset,
+        subset_indices=val_subset_ids,
+        transform=validation_transform,
+    )
+
+    NUM_VERI_TRAIN_CLASSES = int(576 * (1.0 - VAL_PERCENT))
 
     # Create criterion metrics with number of classes matching number of ids in VeRi train set
     # Using [NormalizedSoftmaxLoss](https://kevinmusgrave.github.io/pytorch-metric-learning/losses/#normalizedsoftmaxloss)
@@ -66,21 +136,9 @@ if __name__ == "__main__":
 
     miner = miners.MultiSimilarityMiner()
 
-    # TODO: Do more research on transformations
-    transform = T.Compose(
-        [
-            T.Resize((256, 256)),
-            T.RandomHorizontalFlip(),
-            T.ToTensor(),
-            T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-        ]
-    )
-
     # Load train split from VeRi dataset
-    train_dataset = VeRiDataset(
-        img_dir="../datasets/VeRi/image_train/", transform=transform
-    )
     train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True, num_workers=8)
+    val_loader = DataLoader(val_dataset, batch_size=32, shuffle=False, num_workers=8)
 
     # Checkpoint callback
     checkpoint_callback = ModelCheckpoint(
